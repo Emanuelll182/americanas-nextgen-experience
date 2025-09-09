26b666f2814e571257c5ec0596fe170aabb2ac84
Emanuelll182 <154562224+Emanuelll182@users.noreply.github.com>
2025-09-09 18:09:50 -0300

    Update FeaturedProducts.tsx

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -186,7 +186,7 @@ const FeaturedProducts = () => {
             </>
           )}
           
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 px-12 md:px-0">
+          <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-3 sm:gap-6 px-8 sm:px-12 md:px-0">
             {currentProducts.map((product) => (
             <Card 
               key={product.id} 
